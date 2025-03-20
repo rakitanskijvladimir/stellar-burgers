1d1909342e0f8a9aa1e917104499755936d34a05
vladimir <rakitanskijvladimir4@gmail.com>
2025-03-20 11:35:31 +0300

    частичные исправления

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,34 @@
 import { defineConfig } from 'cypress';
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: 'cypress/support/index.ts',
-    fixturesFolder: 'cypress/fixtures',
-    specPattern: 'cypress/e2e/**/*.cy.ts',
-    video: false,
+    baseUrl: 'http://localhost:3000/',
+    
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Шаблон для поиска тестов
+    async setupNodeEvents(on, config) {
+      const options = {
+        webpackOptions: {
+          resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+          module: {
+            rules: [
+              {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+              },
+            ],
+          },
+          plugins: [
+            new NodePolyfillPlugin(), // Полифил для других Node.js-модулей
+          ],
+        },
+      };
+      on('file:preprocessor', webpackPreprocessor(options));
+      return config;
+    },
   },
 });
--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8'
 };
+
+export default config;
